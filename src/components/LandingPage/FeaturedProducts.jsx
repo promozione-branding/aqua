470426ea4db8d6b496fd421c2969fab770ade965
algoknightcode <algoknightcode@gmail.com>
2026-08-05 11:26:37 +0530

    C40

--- src/components/LandingPage/FeaturedProducts.jsx
+++ src/components/LandingPage/FeaturedProducts.jsx
@@ -1,5 +1,6 @@
 import connectDB from "@/config/connectDB";
 import Product from "@/models/Product/Product";
+import Category from "@/models/category/Category";
 import ScrollDrivenHorizontal from "@/components/UI/ScrollDrivenHorizontal";
 
 export default async function FeaturedProducts() {
