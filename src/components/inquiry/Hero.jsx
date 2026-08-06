9ef9579818079f92f575bb820ce1efdaba759295
manish kumar <manishkumarstudent8744@gmail.com>
2026-08-06 11:10:14 +0530

    hero chnages inquiry

--- src/components/inquiry/Hero.jsx
+++ src/components/inquiry/Hero.jsx
@@ -1,32 +1,38 @@
-"use client"
-import React, { useState } from 'react'
-import Popup from '../Popup';
-import Image from 'next/image';
+"use client";
+import React, { useState } from "react";
+import Popup from "../Popup";
+import Image from "next/image";
 
 export default function Hero() {
-        const [isFormOpen, setIsFormOpen] = useState(false);
+  const [isFormOpen, setIsFormOpen] = useState(false);
 
   return (
     <>
-    <section onClick={()=>{setIsFormOpen(true)}} className="w-full ">
-  <Image
-  height={100} width={100}
-    src="/inquiry/desktop.webp"
-    alt="RO Banner"
-    className="w-full hidden md:block h-full md:h-[80vh] object-cover"
-  />
-  <Image
-    src="/inquiry/mobb.webp"
-    alt="RO Banner"
-    height={100} width={100}
-    className="w-full md:hidden  h-[37vh] object-cover"
-  />
-</section>
+      <section
+        onClick={() => {
+          setIsFormOpen(true);
+        }}
+        className="w-full "
+      >
+        <Image
+          height={2000}
+          width={2000}
+          src="/inquiry/desktop.webp"
+          alt="RO Banner"
+          className="w-full hidden md:block h-full md:h-[80vh] object-cover"
+        />
+        <Image
+          src="/inquiry/mobb.webp"
+          alt="RO Banner"
+          height={100}
+          width={100}
+          className="w-full md:hidden  h-[37vh] object-cover"
+        />
+      </section>
 
-
- {isFormOpen && (                   
-      <Popup isOpen={isFormOpen} onClose={() => setIsFormOpen(false)}/>
-         )}
+      {isFormOpen && (
+        <Popup isOpen={isFormOpen} onClose={() => setIsFormOpen(false)} />
+      )}
     </>
-  )
+  );
 }
